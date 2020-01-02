b4d664e9cf6539c29daf216e099902898a4edbb9
Ying Dong <dongyingname@yahoo.com>
2020-01-01 17:36:28 -0800

    Add lifecycle log callbacks to both of the activities

--- TwoActivities2/app/src/main/java/com/example/twoactivities2/SecondActivity.java
+++ TwoActivities2/app/src/main/java/com/example/twoactivities2/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.twoactivities2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,11 +13,14 @@ public class SecondActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY =
             "com.example.android.twoactivities.extra.REPLY";
     private EditText mReply;
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
         setContentView(R.layout.activity_second);
         mReply = findViewById(R.id.editText_second);
         Intent intent = getIntent();
@@ -30,6 +34,38 @@ public class SecondActivity extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK,replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 }
